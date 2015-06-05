855c2e2eca15294d0a30e7d5cf645fd6f9cd880c
Neerav Vadodaria <neerav.sv30@gmail.com>
2015-06-05 21:53:59 +0530

    Updated the GameOfThronesI file

--- src/main/java/com/njava/hackerrank/algorithms/string/GameofThronesI.java
+++ src/main/java/com/njava/hackerrank/algorithms/string/GameofThronesI.java
@@ -1,11 +1,38 @@
 package com.njava.hackerrank.algorithms.string;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
 
 public class GameofThronesI {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        final String line = br.readLine().toLowerCase();
+		final String line = br.readLine().toLowerCase();
+		Map<Character, Integer> charMap = new HashMap<Character, Integer>(
+				Math.min(line.length(), 26));
+
+		for (int i = 0; i < line.length(); ++i) {
+			char charAt = line.charAt(i);
+			if (!charMap.containsKey(charAt)) {
+				charMap.put(charAt, 1);
+			} else {
+				charMap.put(charAt, charMap.get(charAt) + 1);
+			}
+		}
+		int oddCount = 0;
+		for(Integer count: charMap.values()) {
+			if((count % 2) != 0)
+				oddCount++;
+		}
+		if(oddCount > 1)
+			System.out.println("NO");
+		else
+			System.out.println("YES");
 	}
 }
+
+/**
+ * aaabbbb
+ */
